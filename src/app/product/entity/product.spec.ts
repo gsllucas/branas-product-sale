3b1005f502dfd937f6a3dacc055496b46bd0393c
gsllucas <llucasgs@gmail.com>
2023-01-25 22:38:50 -0300

    :white_check_mark: Test Product entity

---
+++ src/app/product/entity/product.spec.ts
@@ -0,0 +1,22 @@
+import { test, describe, expect } from 'vitest';
+import { Product } from './product';
+
+describe('Product Entity Test', () => {
+  test('It must not create a Product with amount as zero', () => {
+    expect(
+      () => new Product('Keyboard', 'TKL RGB Keyboard', 299.99, 0)
+    ).toThrowError('It is necessary at least one product amount');
+  });
+
+  test('It must not create a Product blank name', () => {
+    expect(() => new Product('', 'TKL RGB Keyboard', 239, 20)).toThrowError(
+      'Missing product field'
+    );
+  });
+
+  test('It must not create a Product blank description', () => {
+    expect(() => new Product('Keyboard', '', 239, 20)).toThrowError(
+      'Missing product field'
+    );
+  });
+});
